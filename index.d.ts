77842de7c89fe2656d40438c793665fb5bbcd9f8
mike galakos <mgalakos@eoppep.gr>
2021-11-30 08:29:30 +0200

    types: namespace esma

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,54 @@
 import type http from 'http'
 
 declare const _default: {
-  createServer: () => EsmaServer
-  static: (root: string, options?: object) => Handler
+  createServer: () => esma.Server
+  static: (root: string, options?: object) => esma.Handler
 }
 export default _default
 
-export const router: () => Router
-export const Router: () => Router
+export const router: () => esma.Router
+export const Router: () => esma.Router
 
 
 
 declare global {
   
-  type EsmaRequest = http.IncomingMessage & {
-    originalUrl: string
-    url: string
-    baseUrl: string
-  }
+  namespace esma {
 
-  type EsmaResponse = http.ServerResponse & {
-    writableEnded: boolean
-  }
+    type Request = http.IncomingMessage & {
+      originalUrl: string
+      url: string
+      baseUrl: string
+    }
 
-  type Middleware = {
-    path: string
-    method: string
-    handlers: Handler[]
-    router: Router
-    urlPattern: any
-    applyHandlers(req: EsmaRequest, res: EsmaResponse): Promise<void | object>
-  }
+    type Response = http.ServerResponse & {
+      writableEnded: boolean
+    }
 
-  type Handler = Router | ((req: EsmaRequest, res: EsmaResponse, next?: Function) => Promise<object> | void)
+    type Middleware = {
+      path: string
+      method: string
+      handlers: Handler[]
+      router: Router
+      urlPattern: any
+      applyHandlers(req: Request, res: Response): Promise<void | object>
+    }
 
-  type EsmaServer = http.Server & Router
+    type Handler = Router | ((req: Request, res: Response, next?: Function) => Promise<object> | void)
 
-  type Router = {
-    type: string
-    mountpath: string
-    middleware: Middleware[]
-    use(path?: string | Handler, ...handlers: Handler[]): void
-    handleRequest(req: EsmaRequest, res: EsmaResponse): Promise<void | object>
-    setHttpMethodHandler(method: string): void
-    toJSON(): object
+
+    type Router = {
+      type: string
+      mountpath: string
+      middleware: Middleware[]
+      use(path?: string | Handler, ...handlers: Handler[]): void
+      handleRequest(req: Request, res: Response): Promise<void | object>
+      setHttpMethodHandler(method: string): void
+      toJSON(): object
+    }
+
+    type Server = http.Server & Router
+
   }
 
 }
