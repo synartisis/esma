e146dfed94469d6b5894556ad1b475bd32fd6113
mike galakos <mgalakos@eoppep.gr>
2024-05-10 17:10:47 +0300

    clean

--- lib/ambient.d.ts
+++ lib/ambient.d.ts
@@ -3,28 +3,11 @@ import type * as types from './esma.d.ts'
 
 declare global {
 
-  namespace esma {
-    // type HttpMethods = types.HttpMethods
+  type Context = {
+    settings: types.Settings
+    express_action: string | null
+    express_result: any
+  }
 
-    // type Request = types.Request
-    // type Response = types.Response
-    // type Session = types.Session
-    // type SessionData = types.SessionData
-    // type StaticOptions = types.StaticOptions
-    // type StaticFileHandler = types.StaticFileHandler
-
-    // type Handler = types.Handler
-    // type ErrorHandler = types.ErrorHandler
-    // type HandlerResult = types.HandlerResult
-
-  }  
-
-    type Context = {
-      settings: types.Settings
-      express_action: string | null
-      express_result: any
-    }
-      
-
-}  
+}
 
